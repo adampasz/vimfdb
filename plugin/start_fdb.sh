3c1d09095d3701192bcc0ce752b360cf19608560
Adam Pasztory <avatar@pasz.com>
2014-06-21 06:58:32 -0700

    Prevent start_fdb from launching a bunch of terminal windows.

--- plugin/start_fdb.sh
+++ plugin/start_fdb.sh
@@ -1,17 +1,18 @@
 #Start FDB in new terminal window and start listening for something to debug
+#This script assumes you are using the terminal only as a debugger window.
+#i.e.: In my setup, I use iterm as my main terminal.
+#If you depend on the basic terminal for your workflow, you'll probably want to revise this.
 export _JAVA_OPTIONS="-Xms1024m -Xmx1024m -XX:MaxPermSize=512m"
 osascript -e '
 	tell application "Terminal" to activate
-	do shell script "killall Terminal"
+	tell application "Terminal" to quit
 	do shell script "killall adl"
 	delay 1
-	tell application "Terminal" to close window 1
 '
 
 osascript -e '
 	tell application "Terminal"
 		tell application "Terminal" to activate
-		do script ("cd ~")
 		delay 1
 		do script ("fdb")  in window 1
 		tell application "Terminal" to deactivate
@@ -21,3 +22,4 @@ osascript -e '
 #run
 #b FooBar.as:75
 #continue
+
